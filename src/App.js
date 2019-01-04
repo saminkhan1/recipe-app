74fc1c8d35477f2ad4f1a186ae1a3f0374fcb388
Mozammil Khan <saminkhann1@gmail.com>
2019-01-04 03:44:43 -0500

    got default value to show up through api calls

--- src/App.js
+++ src/App.js
@@ -2,8 +2,25 @@ import React, { Component } from 'react';
 import './App.css';
 import Form from './components/Form'
 
+const AppID = "571add26";
+const AppKeys = "f92da23c8bcee80bdb03941f1ad781e8";
+
 class App extends Component {
-  
+  state = {
+    recipes: []
+  }
+
+
+   
+  getRecipe = async (e) => {
+    const recipeName = e.target.elements.recipeName.value;
+    e.preventDefault();
+    const api_call = await fetch(`https://api.edamam.com/search?q=chicken&app_id=${AppID}&app_key=${AppKeys}&from=0&to=10`);
+    const data = await api_call.json();
+    this.setState({recipes: data.hits});
+    console.log(this.state.recipes);
+  }
+
 
   render() {
     return (
@@ -11,7 +28,12 @@ class App extends Component {
         <header className="App-header">
           <h1 className="App-title">Recipe Search</h1>
         </header>
+        <Form getRecipe={this.getRecipe}/>
+        {this.state.recipes.map((indivisual) => {
+          return <p key={indivisual.recipe.url}>{indivisual.recipe.label}</p>
+        }) }
       </div>
+      
     );
   }
 }
